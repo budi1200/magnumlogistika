e8928818ac1dc271734f4fd977b9abd036dfc3ff
budi1200 <alenab22@gmail.com>
2018-11-28 21:53:40 +0100

    add image to why section

--- src/components/HomeZakaj.js
+++ src/components/HomeZakaj.js
@@ -6,14 +6,17 @@ export default class HomeZakaj extends Component{
 		const lang = this.props.currentLang;
 
 		return(
-			<div className='section-wrapper'>
+			<div style={{maxWidth: '100%', margin: 0}} className='section-wrapper'>
 				<h1 className='section-title'>{lang.HomeZakaj.title}</h1>
-				<p>{lang.HomeZakaj.desc}</p>
-				<ul>
-					{lang.HomeZakaj.razlogi.map((razlog, index) => {
-						return(<li key={index}>{razlog}</li>);
-					})}
-				</ul>
+				{/*<p>{lang.HomeZakaj.desc}</p>*/}
+				<div className="homezakaj-inner-wrapper">
+					<ul>
+						{lang.HomeZakaj.razlogi.map((razlog, index) => {
+							return(<li key={index}>{razlog}</li>);
+						})}
+					</ul>
+					<img style={{height: 300}} src="./assets/img/why.jpg"/>
+				</div>
 			</div>
 		);
 	}
