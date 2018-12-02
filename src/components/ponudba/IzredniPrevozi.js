8f4c59ae3346e51d74f5a1ed8e11d1da08bfee4b
budi1200 <alenab22@gmail.com>
2018-12-02 23:01:01 +0100

    add slideshow

--- src/components/ponudba/IzredniPrevozi.js
+++ src/components/ponudba/IzredniPrevozi.js
@@ -6,7 +6,18 @@ import HeaderImage from '../HeaderImage';
 
 import Footer from '../Footer';
 
+import SlideShow from 'react-image-show';
+
 export default class IzredniPrevozi extends Component{
+
+	constructor(props){
+		super(props);
+
+		this.state = {
+			slideshow: ['/assets/img/izredni.jpg', '/assets/img/izredni2.jpg']
+		}
+	}
+
 	render(){
 
 		const lang = this.props.currentLang;
@@ -17,9 +28,9 @@ export default class IzredniPrevozi extends Component{
 
         <HeaderImage bgVideo='/assets/vid/ML3.mp4' text={lang.IzredniPrevozi.title} video={true}/>
 				
-				<div className="section-wrapper">
+				<div className="section-wrapper izredni">
 					<span>Posebej previdno in natančno preučimo in načrtujemo pretovorne manipulacije ter prevozno pot. Uredimo vse potrebno, da tovor na prevozni poti ustrezno zaščitimo in zavarujemo. Skrbno organiziramo potek celotne logistične storitve.</span>
-					<img src='/assets/img/izredni.jpg'/>
+					<SlideShow images={this.state.slideshow} width="920px" imagesWidth="800px" imagesHeight="450px" imagesHeightMobile="56vw" infinite fixedImagesHeight />
 				</div>
 
 				<Footer/>
