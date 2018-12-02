055fe781407126d73a9da9d7f7fa5ed81bdb64ee
budi1200 <alenab22@gmail.com>
2018-12-02 11:25:46 +0100

    remove image padding

--- src/components/HomePodrocje.js
+++ src/components/HomePodrocje.js
@@ -9,17 +9,17 @@ export default class HomePodrocje extends Component{
 		const lang = this.props.currentLang;
 
 		return(
-			<div id='podrocje' className='section-wrapper podrocje'>
+			<div id='podrocje' style={{paddingLeft: 0, paddingRight: 0}} className='section-wrapper podrocje'>
 				<StyleRoot style={{display: "flex", flexDirection: "column"}}>
-					<h1 style={animStyles.fadeInUp} className='section-title'>{lang.HomePodrocje.title}</h1>
+					<h1 style={animStyles.fadeInUp} className='section-title padding20'>{lang.HomePodrocje.title}</h1>
 					<img style={animStyles.fadeIn} src='./assets/img/podrocje.png'/>
-					<p style={animStyles.fadeIn}>
+					<p style={animStyles.fadeIn} className='padding20'>
 						{lang.HomePodrocje.desc}
 					</p>
-					<p style={animStyles.fadeIn}>
+					<p style={animStyles.fadeIn} className='padding20'>
 						{lang.HomePodrocje.desc2}
 					</p>
-					<p style={animStyles.fadeIn}>
+					<p style={animStyles.fadeIn} className='padding20'>
 						{lang.HomePodrocje.desc3}
 					</p>
 				</StyleRoot>
